ebe8db5961c2b8c93004d41e6aa0dde702d8c342
Dominik Obermaier <dominik.obermaier@dc-square.de>
2013-08-23 11:46:44 +0200

    updated copyright

--- src/main/java/com/dcsquare/hivemq/plugin/DenyWildcardPluginModule.java
+++ src/main/java/com/dcsquare/hivemq/plugin/DenyWildcardPluginModule.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 dc-square GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.dcsquare.hivemq.plugin;
 
 import com.dcsquare.hivemq.spi.HiveMQPluginModule;
@@ -6,7 +22,7 @@ import com.dcsquare.hivemq.spi.plugin.meta.Information;
 import com.google.inject.Provider;
 import org.apache.commons.configuration.AbstractConfiguration;
 
-import static com.dcsquare.hivemq.spi.config.Configurations.*;
+import static com.dcsquare.hivemq.spi.config.Configurations.noConfigurationNeeded;
 
 
 @Information(name = "HiveMQ DenyWildcard Plugin", author = "Florian Limpoeck", version = "1.0")
